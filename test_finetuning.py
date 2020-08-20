9a11ccade7ba6f119f368c6aee879100700d1f65
vishakhpk <vishakhpadmakumar@gmail.com>
2020-08-20 09:51:06 -0400

    Test finetuning

--- test_finetuning.py
+++ test_finetuning.py
@@ -13,3 +13,5 @@ from smaberta import TransformerModel
 model = TransformerModel('roberta', 'roberta-base', finetune=True, args={"num_train_epochs":1, 'fp16':False})
 
 model.finetune("./data/lm_train", "./data/lm_eval")
+
+model.lm_evaluate()
